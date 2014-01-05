2af0f9d2b540e7beaa8bdcb16d50aafa1f88ec58
rezaur <rezaurc@gmail.com>
2014-01-06 04:02:52 +0600

    test plugin

--- assets/plugins/work-mport/work-mport.php
+++ assets/plugins/work-mport/work-mport.php
@@ -2,16 +2,16 @@
 /*
 Plugin Name: work-mport
 Plugin URI: 
-Description: 
+Description: A very simple startup Plugin
 Version: 0.0.1
 Author: Rezaur Chowdhury
 Author URI: http://www.rezaur.com
 License: GPLv2
 */
-function linkNetBeans($text) {
+function linkbeans($text) {
     $help = "<a href='http://www.netbeans.org'>NetBeans</a>";
     return str_replace('NetBeans', $help, $text);
 }
 
-add_filter('the_content', linkNetBeans);
+add_filter('the_content',  'linkbeans');
 
